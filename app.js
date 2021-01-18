c3a4cb62f16b974c06c4f4a6e2e2dd3f5e151810
Arnab Maji <arnabmaji1999@gmail.com>
2021-01-18 21:44:21 +0530

    Show feedback when user submits a question
    Show corrent or wrong feedback alert when
    user submmits a question and disable the
    submit button on successful submission

--- app.js
+++ app.js
@@ -1,82 +1,94 @@
 let currentQuestion = {
-  statement: "This is Question Statement",
-  options: [
-    "This is Option A",
-    "This is Option B",
-    "This is Option C",
-    "This is Option D",
-  ],
-  correctOptionIndex: 1,
-  score: 10,
+    statement: "This is Question Statement",
+    options: [
+        "This is Option A",
+        "This is Option B",
+        "This is Option C",
+        "This is Option D",
+    ],
+    correctOptionIndex: 1,
+    score: 10,
 };
 
 displayQuestion(currentQuestion);
 
 function displayQuestion(question) {
-  // Display question and its all available options on the page
+    // Display question and its all available options on the page
 
-  let questionStatmentDiv = document.getElementById("question-statement");
-  let optionsDiv = document.getElementById("options");
+    let questionStatmentDiv = document.getElementById("question-statement");
+    let optionsDiv = document.getElementById("options");
 
-  questionStatmentDiv.innerHTML = question.statement;
-  // map each option to a radio input element
-  question.options
-    .map((option) => {
-      // Create radio input for current option
-      let radionInput = document.createElement("input");
-      radionInput.setAttribute("type", "radio");
-      radionInput.setAttribute("name", "radio-option");
-      radionInput.classList.add("form-check-input");
+    questionStatmentDiv.innerHTML = question.statement;
+    // map each option to a radio input element
+    question.options
+        .map((option) => {
+            // Create radio input for current option
+            let radionInput = document.createElement("input");
+            radionInput.setAttribute("type", "radio");
+            radionInput.setAttribute("name", "radio-option");
+            radionInput.classList.add("form-check-input");
 
-      let optionNameNode = document.createTextNode(option);
+            let optionNameNode = document.createTextNode(option);
 
-      // create label for current radion input
-      let labelElement = document.createElement("label");
-      labelElement.classList.add("form-check-label");
+            // create label for current radion input
+            let labelElement = document.createElement("label");
+            labelElement.classList.add("form-check-label");
 
-      // append radio input to the label
-      labelElement.appendChild(radionInput);
-      labelElement.appendChild(optionNameNode);
+            // append radio input to the label
+            labelElement.appendChild(radionInput);
+            labelElement.appendChild(optionNameNode);
 
-      // create a parent div and add label element inside it
-      let parentDiv = document.createElement("div");
-      parentDiv.classList.add("form-check");
+            // create a parent div and add label element inside it
+            let parentDiv = document.createElement("div");
+            parentDiv.classList.add("form-check");
 
-      parentDiv.appendChild(labelElement);
-      return parentDiv;
-    })
-    .forEach((e) => optionsDiv.appendChild(e));
+            parentDiv.appendChild(labelElement);
+            return parentDiv;
+        })
+        .forEach((e) => optionsDiv.appendChild(e));
 }
 
 function getSelectedOption() {
-  /*
-   * Get the selected option from the 'options' div
-   * Returns Index of the selected option
-   * Returns -1 in case, user has not selected any option
-   */
+    /*
+     * Get the selected option from the 'options' div
+     * Returns Index of the selected option
+     * Returns -1 in case, user has not selected any option
+     */
 
-  // Get all inputs of type radio from the div with id 'options'
-  let options = document.querySelectorAll("#options input[type='radio']");
-  for (const optionIndex in options)
-    if (options[optionIndex].checked) return optionIndex;
-  return -1;
+    // Get all inputs of type radio from the div with id 'options'
+    let options = document.querySelectorAll("#options input[type='radio']");
+    for (const optionIndex in options)
+        if (options[optionIndex].checked) return parseInt(optionIndex);
+    return -1;
 }
 
 function onQuestionSubmit() {
-  /*
-   * Process data when user submits a question
-   */
+    /*
+     * Process data when user submits a question
+     */
 
-  // get the selected option
-  let selectedOptionIndex = getSelectedOption();
-  if (selectedOptionIndex === -1) {
-    // In case user has not selected any option
-    // show an error alert on the screen
+    // get the selected option
+    let selectedOptionIndex = getSelectedOption();
+    if (selectedOptionIndex === -1) {
+        // In case user has not selected any option
+        // show an error alert on the screen
+        setQuestionFeebackAlert("Please select any option!", "alert-warning");
+        return;
+    }
+
+    // Validate if the user has selected the correct option
+    if (selectedOptionIndex === currentQuestion.correctOptionIndex)
+        setQuestionFeebackAlert("Correct answer!", "alert-success");
+    else setQuestionFeebackAlert("Wrong answer!", "alert-danger");
+
+    // disable the submit button
+    document.getElementById("question-submit-button").disabled = true;
+}
+
+function setQuestionFeebackAlert(message, className) {
+    // Update question feedback alert as per params
     let feedbackAlert = document.getElementById("question-feedback-alert");
-    feedbackAlert.classList.remove("alert-primary");
-    feedbackAlert.classList.add("alert-danger");
-    feedbackAlert.innerHTML = "Please select an option!";
-    return;
-  }
-  console.log("Selected option index: ", selectedOptionIndex);
+    feedbackAlert.className = "";
+    feedbackAlert.classList.add("alert", className);
+    feedbackAlert.innerHTML = message;
 }
